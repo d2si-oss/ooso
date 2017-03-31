e371656e54d5759aee5775cbcfa528cfae999982
othmane <othmane.nahyl@d2-si.eu>
2017-03-31 11:33:15 +0200

    Remove unnecessary files

--- lambda-kinesis/terraform/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-cd ..
-mvn clean
-mvn package
-
-cd terraform
-terraform apply
--- mr_job/terraform/variables.tf
+++ mr_job/terraform/variables.tf
@@ -1,10 +1,11 @@
 variable "access_key" {
+  default = "AKIAIANZLIOA5RCV2C7A"
 }
+
 variable "secret_key" {
+  default = "upvabmahzYg7CyWsM9lPq/W7DkbfGiKfWSnEKSvq"
 }
+
 variable "region" {
   default = "eu-west-1"
-}
-variable "source_stream" {
-  default = "othmane_source_stream"
 }
