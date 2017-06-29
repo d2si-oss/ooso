de001d46be85797f223896e7544a6f57be4139f9
othmane <othmane.nahyl@d2-si.eu>
2017-06-29 17:57:55 +0200

    Add examples to the new version

--- examples/ad-hoc-example/src/main/java/job/JobLauncher.java
+++ examples/ad-hoc-example/src/main/java/job/JobLauncher.java
@@ -4,7 +4,7 @@ import fr.d2si.ooso.launcher.Launcher;
 import mapper.Mapper;
 import reducer.Reducer;
 
-public class LaunchJob {
+public class JobLauncher {
     public static void main(String[] args) {
         new Launcher()
                 .withMapper(new Mapper())
